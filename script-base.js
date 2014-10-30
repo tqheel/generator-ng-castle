8f508d54a1ac9c84fc42c12720d0fac273fa3b7f
Todd Qualls <tqualls@gmail.com>
2014-10-29 23:10:21 -0400

    Deletes files I dont think are needed to just generate controller files.

--- script-base.js
+++ script-base.js
@@ -31,34 +31,8 @@ var Generator = module.exports = function Generator() {
     this.options.appPath = this.env.options.appPath;
   }
 
-  if (typeof this.env.options.testPath === 'undefined') {
-    try {
-      this.env.options.testPath = require(path.join(process.cwd(), 'bower.json')).testPath;
-    } catch (e) {}
-    this.env.options.testPath = this.env.options.testPath || 'test/spec';
-  }
-
-  this.env.options.coffee = this.options.coffee;
-  if (typeof this.env.options.coffee === 'undefined') {
-    this.option('coffee');
-
-    // attempt to detect if user is using CS or not
-    // if cml arg provided, use that; else look for the existence of cs
-    if (!this.options.coffee &&
-      this.expandFiles(path.join(this.env.options.appPath, '/scripts/**/*.coffee'), {}).length > 0) {
-      this.options.coffee = true;
-    }
-
-    this.env.options.coffee = this.options.coffee;
-  }
-
   var sourceRoot = '/templates/javascript';
   this.scriptSuffix = '.js';
-
-  if (this.env.options.coffee) {
-    sourceRoot = '/templates/coffeescript';
-    this.scriptSuffix = '.coffee';
-  }
 
   this.sourceRoot(path.join(__dirname, sourceRoot));
 };
@@ -72,52 +46,11 @@ Generator.prototype.appTemplate = function (src, dest) {
   ]);
 };
 
-Generator.prototype.testTemplate = function (src, dest) {
-  yeoman.generators.Base.prototype.template.apply(this, [
-    src + this.scriptSuffix,
-    path.join(this.env.options.testPath, dest.toLowerCase()) + this.scriptSuffix
-  ]);
-};
-
-Generator.prototype.htmlTemplate = function (src, dest) {
-  yeoman.generators.Base.prototype.template.apply(this, [
-    src,
-    path.join(this.env.options.appPath, dest.toLowerCase())
-  ]);
-};
-
-Generator.prototype.addScriptToIndex = function (script) {
-  try {
-    var appPath = this.env.options.appPath;
-    var fullPath = path.join(appPath, 'index.html');
-    angularUtils.rewriteFile({
-      file: fullPath,
-      needle: '<!-- endbuild -->',
-      splicable: [
-        '<script src="scripts/' + script.toLowerCase().replace(/\\/g, '/') + '.js"></script>'
-      ]
-    });
-  } catch (e) {
-    this.log.error(chalk.yellow(
-      '\nUnable to find ' + fullPath + '. Reference to ' + script + '.js ' + 'not added.\n'
-    ));
-  }
-};
-
-Generator.prototype.generateSourceAndTest = function (appTemplate, testTemplate, targetDirectory, skipAdd) {
+Generator.prototype.generateSourceAndTest = function (appTemplate, targetDirectory) {
   // Services use classified names
   if (this.generatorName.toLowerCase() === 'service') {
     this.cameledName = this.classedName;
   }
-  if(this.generatorName.toLowerCase() === 'controller'){
-    this.cameledName = this.cameledName.replace('-','');
-    this.classedName = this.classedName.replace('-','');
-    this.name = this.name.toLowerCase();
-  }
 
-  this.appTemplate(appTemplate, path.join('scripts', targetDirectory, this.name + '.controller'));
-  this.testTemplate(testTemplate, path.join(targetDirectory, this.name));
-  if (!skipAdd) {
-    this.addScriptToIndex(path.join(targetDirectory, this.name));
-  }
+  this.appTemplate(appTemplate, path.join('scripts', targetDirectory, this.name));
 };
